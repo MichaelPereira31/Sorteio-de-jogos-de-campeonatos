5d220781068a6a21f517de093ecab4861122e756
Michael Pereira <michael.pereira@chat2desk.com.br>
2023-06-18 21:16:22 -0300

    feat: add cors

--- src/shared/infra/http/server.ts
+++ src/shared/infra/http/server.ts
@@ -15,6 +15,12 @@ import '../../containers';
 import '../database';
 
 const app = express();
+app.use((req, res, next) => {
+  res.setHeader('Access-Control-Allow-Origin', 'http://localhost:3000');
+  res.setHeader('Access-Control-Allow-Methods', 'GET, POST, PUT, DELETE');
+  res.setHeader('Access-Control-Allow-Headers', 'Content-Type');
+  next();
+});
 
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
